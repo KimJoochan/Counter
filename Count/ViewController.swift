96b7c6a5093b4c4871d3eb9ba481d3cb6cd6d171
dit02 <dit@imac-7.local>
2019-03-20 16:34:45 +0900

    123

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     var counter=0;
     override func viewDidLoad() {
         super.viewDidLoad()
+        txt.text="0"
         // Do any additional setup after loading the view, typically from a nib.
     }
     @IBAction func tapCount(_ sender: UIButton) {
