0326a5ceb275d2924978e7fe6b22e07b0cb8fb4b
dit02 <dit@imac-7.local>
2019-03-21 12:48:21 +0900

    012345678987654321 button

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -17,21 +17,23 @@ class ViewController: UIViewController {
         txt.text="0"
         // Do any additional setup after loading the view, typically from a nib.
     }
-    var checked=0
+    var checked=0 // 체크용 변수를 0으로 선언
     @IBAction func tapCount(_ sender: UIButton) {
+    
+        
         if(checked==0){
             counter = counter+1;
             if(counter>=9){
-                checked=1;
+                checked=1; //9까지 완료되면 체크용 변수를 1로 바꿈
             }
-        }else if(checked==1){
-            counter = 0
-            checked=0;
-            
+        }else if(checked==1){ // 체크변수가 1이되면 다시 1씩 빼서 0까지 만듬
+            counter=counter-1;
+            if(counter<=0){
+                checked=0;
+            }
         }
         txt.text=String(counter)
     }
     
-
 }
 
